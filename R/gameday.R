e8b7a4ec027b4e5a64f2b083304b5e0899df1cf9
Wenyi <wenyixia6235@gmail.com>
2014-11-12 11:22:22 -0800

    first R package

---
+++ R/gameday.R
@@ -0,0 +1,10 @@
+#' My first R package: gameday
+#'
+#' A one-line sentence on what this packages does
+#'
+#' Single paragraph with more detail on the package.
+#' This can span several lines.
+#'
+#' @docType package
+#' @name gameday
+NULL
